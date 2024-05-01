7e6a0a854ef55018c78326736e84af2b261d79b0
LaineyCH <hen20028@byui.edu>
2024-05-01 22:15:07 +0400

    Add comments

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,10 +5,12 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 const moddate = new Date(document.lastModified);
 document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
 
-
+//select HTML elements and assign to constants
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('nav');
 
+//create event listener for the hamburger menu button
+//toggle the open class on/off for the nav menu and button, when the button is clicked
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
