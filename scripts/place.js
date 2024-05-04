38e51c73ccb4822e2e0d9e28013455f1ad54240a
LaineyCH <hen20028@byui.edu>
2024-05-04 12:43:19 +0400

    correct wind chill functiion

--- scripts/place.js
+++ scripts/place.js
@@ -10,8 +10,7 @@ let temp = 32;
 let wind = 11;
 
 function calculateWindChill(temperature, windSpeed) {
-    let windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
-    return windChill.toFixed(1) + "°C";
+    return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1) + "°C";
 };
 
 document.querySelector("#wind-chill").textContent = (temp <= 10 && wind >= 4.8) ? calculateWindChill(temp, wind) : "N/A";
