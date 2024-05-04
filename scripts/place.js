3d7bae268c7fd5092ceafa9a8ef2b0084a64a899
LaineyCH <hen20028@byui.edu>
2024-05-04 08:46:57 +0400

    add wind chill

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,14 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 //get the date the document was last modified
 const moddate = new Date(document.lastModified);
 document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
+
+
+let temp = 32;
+let wind = 11;
+
+function calculateWindChill(temperature, windSpeed) {
+    let windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    return windChill.toFixed(1) + "°C";
+};
+
+document.querySelector("#wind-chill").textContent = (temp <= 10 && wind >= 4.8) ? calculateWindChill(temp, wind) : "N/A";
