bac5631a12354520dc472f7f85016611ae157e43
LaineyCH <hen20028@byui.edu>
2024-05-10 08:19:23 +0400

    add lazy load script

--- week04/scripts/lazyload.js
+++ week04/scripts/lazyload.js
@@ -3,3 +3,14 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
 //get the date the document was last modified
 document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
+
+
+// remove the data-src attribute when the image has loaded
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+	img.setAttribute('src', img.getAttribute('data-src'));
+	img.onload = function() {
+		img.removeAttribute('data-src');
+	};
+});
+
+
