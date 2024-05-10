f0dd21b2d5e43a92ff94e54208d70e65108a00a8
LaineyCH <hen20028@byui.edu>
2024-05-10 19:21:06 +0400

    add object ponder activity

--- week04/scripts/lazyload.js
+++ week04/scripts/lazyload.js
@@ -5,12 +5,10 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
 
 
-// remove the data-src attribute when the image has loaded
-[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
-	img.setAttribute('src', img.getAttribute('data-src'));
-	img.onload = function() {
-		img.removeAttribute('data-src');
-	};
+// remove the data-src attribute when the image has loaded (see notes)
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
 });
-
-
