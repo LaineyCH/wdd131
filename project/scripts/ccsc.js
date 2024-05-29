80e114147eac723bd1e9580aba933aed2ed06952
LaineyCH <hen20028@byui.edu>
2024-05-29 20:10:28 +0200

    update ccsc project

--- project/scripts/ccsc.js
+++ project/scripts/ccsc.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 //get the date the document was last modified
 document.getElementById("lastmodified").innerHTML = new Date(document.lastModified);
 
+//select HTML elements and assign to constants
+const hamButton = document.querySelector('#mobile-menu-button');
+const navigation = document.querySelector('nav');
+
+//create event listener for the hamburger menu button
+//toggle the open class on/off for the nav menu and button, when the button is clicked
+hamButton.addEventListener('click', () => {
+    hamButton.classList.toggle('open');
+    navigation.classList.toggle('open');
+});
